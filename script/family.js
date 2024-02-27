dec99df105b7599f688b401bad6ee54199d62270
Efimova Olga <Olga.Efimova@siriusxm.com>
2024-02-27 16:19:31 +0300

    family galery

---
+++ script/family.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".img-conteiner img").forEach((img) => {
+  img.onclick = () => {
+    document.querySelector(".pop-up").style.display = "block";
+    document.querySelector(".pop-up img").src = img.getAttribute("src");
+  };
+});
+
+document.querySelector(".pop-up span").onclick = () => {
+  document.querySelector(".pop-up").style.display = "none";
+};
